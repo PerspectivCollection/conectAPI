cb7d72cd9df90433b27dc1059820f1068a000153
sivert <sivert.ostgard@hiof.no>
2022-03-04 15:37:18 +0100

    feet: new folder

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -7,11 +7,16 @@ public class Main : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+   Debug.Log("Hello, World");
     }
 
     // Update is called once per frame
     void Update()
+    {
+    
+    }
+
+    private void FixedUpdate()
     {
         
     }
